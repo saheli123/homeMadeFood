de288be457258fbfd3c744f7312d0cf129d34f1d
Saheli <solu.nly@gmail.com>
2021-01-26 22:32:41 -0800

    added api key from env

--- app/User.php
+++ app/User.php
@@ -2,53 +2,53 @@
 
 namespace App;
 
-use Laravel\Passport\HasApiTokens;
+use App\Notifications\VerifyApiEmail;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use App\Notifications\VerifyApiEmail;
+use Laravel\Passport\HasApiTokens;
 use Laravel\Scout\Searchable;
-
 
 class User extends Authenticatable implements MustVerifyEmail
 {
-    use HasApiTokens, Notifiable,Searchable;
+    use HasApiTokens, Notifiable, Searchable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+/**
+ * The attributes that are mass assignable.
+ *
+ * @var array
+ */
     protected $fillable = [
-        'name', 'email', 'password','slug','timezone'
+        'name', 'email', 'password', 'slug', 'timezone',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
+/**
+ * The attributes that should be hidden for arrays.
+ *
+ * @var array
+ */
     protected $hidden = [
         'password', 'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
+/**
+ * The attributes that should be cast to native types.
+ *
+ * @var array
+ */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-    public function getNameAttribute($value){
+    public function getNameAttribute($value)
+    {
         return ucfirst($value);
     }
     public function toSearchableArray()
-{
-  $array = $this->toArray();
+    {
+        $array = $this->toArray();
 
-  return array('name' => $array['name']);
-}
+        return array('name' => $array['name']);
+    }
     public function dishes()
     {
         return $this->hasMany(FoodItem::class);
@@ -69,52 +69,49 @@ class User extends Authenticatable implements MustVerifyEmail
     }
     public function ordersbycustomer()
     {
-        return $this->hasManyThrough(\App\OrderProduct::class, \App\FoodItem::class,"user_id","product_id","id");
+        return $this->hasManyThrough(\App\OrderProduct::class, \App\FoodItem::class, "user_id", "product_id", "id");
     }
     public function sendApiEmailVerificationNotification()
     {
         $this->notify(new VerifyApiEmail); // my notification
     }
-    public static function getLocationFromLatLng($lat,$lng){
-       $googleapiUrl="https://maps.google.com/maps/api/geocode/json?key=".env('GOOGLE_API_KEY')."&latlng=".$lat.",".$lng."&sensor=false";
-       $data = file_get_contents($googleapiUrl);
-       $data = json_decode($data);
-        $add_array  = $data->results;
+    public static function getLocationFromLatLng($lat, $lng)
+    {
+        $googleapiUrl = "https://maps.google.com/maps/api/geocode/json?key=" . env('GOOGLE_API_KEY') . "&latlng=" . $lat . "," . $lng . "&sensor=false";
+        $data = file_get_contents($googleapiUrl);
+        $data = json_decode($data);
+        $add_array = $data->results;
 
         $country = "";
         $state = "";
         $city = "";
-        $pincode=00000;
-        $address=[];
-        if(isset($add_array[0])){
+        $pincode = 00000;
+        $address = [];
+        if (isset($add_array[0])) {
             $add_array = $add_array[0];
             $add_array = $add_array->address_components;
-        //$address['fullAddress']=isset($add_array->formatted_address)?$add_array->formatted_address:"";
-        foreach ($add_array as $key) {
-        if($key->types[0] == 'administrative_area_level_2')
-        {
-            $city = $key->long_name;
+//$address['fullAddress']=isset($add_array->formatted_address)?$add_array->formatted_address:"";
+            foreach ($add_array as $key) {
+                if ($key->types[0] == 'administrative_area_level_2') {
+                    $city = $key->long_name;
+                }
+                if ($key->types[0] == 'administrative_area_level_1') {
+                    $state = $key->long_name;
+                }
+                if ($key->types[0] == 'country') {
+                    $country = $key->short_name;
+                }
+                if ($key->types[0] == 'postal_code') {
+                    $pincode = $key->long_name;
+                }
+            }
         }
-        if($key->types[0] == 'administrative_area_level_1')
-        {
-            $state = $key->long_name;
-        }
-        if($key->types[0] == 'country')
-        {
-            $country = $key->short_name;
-        }
-        if($key->types[0] == 'postal_code')
-        {
-            $pincode = $key->long_name;
-        }
-        }
-    }
-        $address["country"]=$country!=""?(\App\Countries::where("sortname",$country)->count()>0?\App\Countries::where("sortname",$country)->first()->id:0):0;
-        $address["city"]=$city!=""?(\App\Cities::where("name",$city)->count()>0?\App\Cities::where("name",$city)->first()->id:0):0;
-        $address["state"]=$state!=""?(\App\States::where("name",$state)->count()>0?\App\States::where("name",$state)->first()->id:0):0;
-        //$address["pincode"]=$pincode;
+        $address["country"] = $country != "" ? (\App\Countries::where("sortname", $country)->count() > 0 ? \App\Countries::where("sortname", $country)->first()->id : 0) : 0;
+        $address["city"] = $city != "" ? (\App\Cities::where("name", $city)->count() > 0 ? \App\Cities::where("name", $city)->first()->id : 0) : 0;
+        $address["state"] = $state != "" ? (\App\States::where("name", $state)->count() > 0 ? \App\States::where("name", $state)->first()->id : 0) : 0;
+//$address["pincode"]=$pincode;
 
         return $address;
-        //echo "Country : ".$country." ,State : ".$state." ,City : ".$city;
+//echo "Country : ".$country." ,State : ".$state." ,City : ".$city;
     }
 }
